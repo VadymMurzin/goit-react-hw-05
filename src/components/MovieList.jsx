26f2351db6323eda9b90e06bdeae5f54f7ffba2a
VadymMurzin <prometey736@gmail.com>
2024-02-24 14:43:20 +0100

    fix Movielist and MovieDetailPage

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,34 @@
-// MovieList.js
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
+
+// const MovieList = ({ movies }) => {
+//   return (
+//     <ul>
+//       {movies.map((movie) => (
+//         <li key={movie.id}>
+//           <NavLink to={`/movies/${movie.id}`}>{movie.title}</NavLink>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
+
+// export default MovieList;
+
+import { NavLink, useLocation } from 'react-router-dom';
 
 const MovieList = ({ movies }) => {
+  const location = useLocation();
+
   return (
     <ul>
       {movies.map((movie) => (
         <li key={movie.id}>
-          <NavLink to={`/movies/${movie.id}`}>{movie.title}</NavLink>
+          <NavLink to={{
+            pathname: `/movies/${movie.id}`,
+            state: { from: location }
+          }}>
+            {movie.title}
+          </NavLink>
         </li>
       ))}
     </ul>
@@ -14,3 +36,4 @@ const MovieList = ({ movies }) => {
 };
 
 export default MovieList;
+
--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -75,9 +75,90 @@
 
 // export default MovieDetailsPage;
 
+// import { useState, useEffect, useRef } from "react";
+// import axios from "axios";
+// import { NavLink, useLocation, Outlet, useParams } from "react-router-dom";
+// import MovieCast from "../components/MovieCast";
+// import MovieReviews from "../components/MovieReviews";
+
+// const MovieDetailsPage = () => {
+//   const [movieDetails, setMovieDetails] = useState(null);
+//   const [showCredits, setShowCredits] = useState(false);
+//   const [showReviews, setShowReviews] = useState(false);
+//   const { movieId } = useParams();
+//   const location = useLocation();
+//   const prevLocation = useRef();
+
+//   useEffect(() => {
+//     const fetchMovieDetails = async () => {
+//       try {
+//         const response = await axios.get(
+//           `https://api.themoviedb.org/3/movie/${movieId}`,
+//           {
+//             headers: {
+//               Authorization:
+//                 "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZjQ5NDkyMjkyMGU4ODIzNTBiMWYwMDcyMGMyYTNlZiIsInN1YiI6IjY1ZDYyNjVkMzNhMzc2MDE4Njc4NTlhMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.osTxCzY6WwMNw1aB8Mz5nm95MVPzZYh63DkU-M6GoUo",
+//             },
+//           }
+//         );
+//         setMovieDetails(response.data);
+//       } catch (error) {
+//         console.error("Error fetching movie details:", error);
+//       }
+//     };
+
+//     fetchMovieDetails();
+//   }, [movieId]);
+
+//   useEffect(() => {
+//     prevLocation.current = location;
+//   }, [location]);
+
+//     const handleToggleCredits = () => {
+//     setShowCredits((prevShowCredits) => !prevShowCredits);
+//   };
+
+//     const handleToggleReviews = () => {
+//     setShowReviews((prevShowReviews) => !prevShowReviews);
+//   };
+  
+
+//   return (
+//     <div>
+//       {movieDetails && (
+//         <div>
+//           <NavLink to={prevLocation.current.pathname}>Go Back</NavLink>
+//           <h2>{movieDetails.title}</h2>
+//           <p>{movieDetails.overview}</p>
+//           <img
+//             src={`https://image.tmdb.org/t/p/w400/${movieDetails.poster_path}`}
+//             alt={movieDetails.title}
+//           />
+
+//           {/* Movie Credits Component */}
+//           <ul>
+//           <NavLink onClick={handleToggleCredits}>Movie Cast</NavLink>
+//             {showCredits && <MovieCast movieId={movieId} />}
+//             <Outlet />
+//           </ul>
+
+//           {/* Movie Reviews Component */}
+//           <ul>
+//           <NavLink onClick={handleToggleReviews}>Movie Reviews</NavLink>
+//           {showReviews && <MovieReviews movieId={movieId} />}
+//             <Outlet />
+//           </ul>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default MovieDetailsPage;
+
 import { useState, useEffect, useRef } from "react";
 import axios from "axios";
-import { NavLink, useLocation, Outlet, useParams } from "react-router-dom";
+import { Link, NavLink, useLocation, Outlet, useParams } from "react-router-dom";
 import MovieCast from "../components/MovieCast";
 import MovieReviews from "../components/MovieReviews";
 
@@ -87,7 +168,7 @@ const MovieDetailsPage = () => {
   const [showReviews, setShowReviews] = useState(false);
   const { movieId } = useParams();
   const location = useLocation();
-  const prevLocation = useRef();
+  const prevLocation = useRef(location.state || {});
 
   useEffect(() => {
     const fetchMovieDetails = async () => {
@@ -111,23 +192,22 @@ const MovieDetailsPage = () => {
   }, [movieId]);
 
   useEffect(() => {
-    prevLocation.current = location;
+    prevLocation.current = location.state || {};
   }, [location]);
 
-    const handleToggleCredits = () => {
+  const handleToggleCredits = () => {
     setShowCredits((prevShowCredits) => !prevShowCredits);
   };
 
-    const handleToggleReviews = () => {
+  const handleToggleReviews = () => {
     setShowReviews((prevShowReviews) => !prevShowReviews);
   };
-  
 
   return (
     <div>
       {movieDetails && (
         <div>
-          <NavLink to={prevLocation.current.pathname}>Go Back</NavLink>
+          <Link to={prevLocation.current.from || "/movies"}>Go Back</Link>
           <h2>{movieDetails.title}</h2>
           <p>{movieDetails.overview}</p>
           <img
@@ -137,15 +217,19 @@ const MovieDetailsPage = () => {
 
           {/* Movie Credits Component */}
           <ul>
-          <NavLink onClick={handleToggleCredits}>Movie Cast</NavLink>
+            <NavLink to={`/movies/${movieId}/cast`} onClick={handleToggleCredits}>
+              Movie Cast
+            </NavLink>
             {showCredits && <MovieCast movieId={movieId} />}
             <Outlet />
           </ul>
 
           {/* Movie Reviews Component */}
           <ul>
-          <NavLink onClick={handleToggleReviews}>Movie Reviews</NavLink>
-          {showReviews && <MovieReviews movieId={movieId} />}
+            <NavLink to={`/movies/${movieId}/reviews`} onClick={handleToggleReviews}>
+              Movie Reviews
+            </NavLink>
+            {showReviews && <MovieReviews movieId={movieId} />}
             <Outlet />
           </ul>
         </div>
